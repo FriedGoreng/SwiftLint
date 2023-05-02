d6d7453ba5ec46f0bfc892201033cbc8dc65be79
JDLembongNation <lembongjoshua@gmail.com>
2023-05-02 15:27:45 +0100

    No more twoples

--- Source/SwiftLintBuiltInRules/Rules/Metrics/LargeTupleRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Metrics/LargeTupleRule.swift
@@ -16,24 +16,26 @@ struct LargeTupleRule: SourceKitFreeRule, ConfigurationProviderRule {
         LargeTupleRuleVisitor(viewMode: .sourceAccurate)
             .walk(file: file, handler: \.violationPositions)
             .sorted(by: { $0.position < $1.position })
-            .compactMap { position, size in
+            .compactMap { position, size, tags in
                 for parameter in configuration.params where size > parameter.value {
-                    let reason = "Tuples should have at most \(configuration.warning) members"
-                    return StyleViolation(ruleDescription: Self.description,
-                                          severity: parameter.severity,
-                                          location: Location(file: file, position: position),
-                                          reason: reason)
+                    if (tags != size) {
+                        let reason = "Tuples should have at most \(configuration.warning) members or have all members labelled"
+                        return StyleViolation(ruleDescription: Self.description,
+                                              severity: parameter.severity,
+                                              location: Location(file: file, position: position),
+                                              reason: reason)
+                    }
                 }
-
                 return nil
             }
     }
 }
 
 private final class LargeTupleRuleVisitor: SyntaxVisitor {
-    private(set) var violationPositions: [(position: AbsolutePosition, memberCount: Int)] = []
+    private(set) var violationPositions: [(position: AbsolutePosition, memberCount: Int, tags: Int)] = []
 
     override func visitPost(_ node: TupleTypeSyntax) {
-        violationPositions.append((node.positionAfterSkippingLeadingTrivia, node.elements.count))
+        let tags = node.elements.compactMap{$0.colon}.count
+        violationPositions.append((node.positionAfterSkippingLeadingTrivia, node.elements.count, tags))
     }
 }
